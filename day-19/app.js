e42b46931055b228c56ee59bc88d230b5368930c
Zignuts Technolab <ztlab13@Zignutss-Mac-mini.local>
2023-02-27 11:16:37 +0530

    user signup

--- day-19/app.js
+++ day-19/app.js
@@ -8,6 +8,7 @@ const app = express();
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.PORT_URI, {
@@ -32,6 +33,7 @@ app.use((req, res, next) => {
 //Routes which should handle requests 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 //error handling
 app.use((req, res, next) => {
